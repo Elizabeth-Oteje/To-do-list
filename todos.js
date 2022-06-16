c9384225929ffc302b5aca29c5dba6e7270793cd
Elizabeth Oteje <lizzyoteje@gmail.com>
2022-06-16 18:55:11 +0100

    first

---
+++ todos.js
@@ -0,0 +1,45 @@
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+
+function addItem (e){
+    e.preventDefault();
+    let displayValue = document.getElementById('item').value;
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+    
+
+    li.append(displayValue);
+
+    var deleteBtn = document.createElement('button')
+
+
+    deleteBtn.className = 'btn btn-danger btn-sm float-end delete';
+
+
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    li.appendChild(deleteBtn);
+    
+    itemList.append(li);
+
+    
+  
+}
+
+
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure you want to remove?')){
+            
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+
+   
+
+}
